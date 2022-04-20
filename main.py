21f5b30e2fc070b408550865ada0939b1c6215cb
Miroslav.Dilov <mirkos354@abv.bg>
2022-04-20 22:11:41 +0300

    Finished All parts of API

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = 'lord_of_the_ringsu'
 
 class MyForm(FlaskForm):
-    choice = SelectField('Route', choices=['book', 'movie', 'character', 'chapter'])
+    choice = SelectField('Route', choices=['book', 'movie', 'character', 'chapter', 'quote'])
 
 
 @app.route('/index')
@@ -20,7 +20,16 @@ def hello_world():
         print(api_request(choice))
         if choice == 'book':
             return redirect(url_for('books'))
+        elif choice == 'movie':
+            return redirect(url_for('movies'))
+        elif choice == 'character':
+            return redirect(url_for('characters'))
+        elif choice == 'chapter':
+            return redirect(url_for('chapters'))
+        elif choice == 'quote':
+            return redirect(url_for('quotes'))
         return redirect('/')
+
     return render_template('index.html', form=form)
 
 
@@ -39,6 +48,56 @@ def book_chapters(id):
 
     return render_template('book_chapters.html', chapters=response['docs'])
 
+
+@app.route('/movies')
+def movies():
+    response = api_request('movie')
+    print(response)
+
+    return render_template('movies.html', movies=response['docs'])
+
+
+@app.route('/movie_info/<id>')
+def movie_info(id):
+    response = api_request(f'movie/{id}')
+
+    print(response)
+
+    return render_template('movie_info.html', movie=response['docs'])
+
+
+@app.route('/movie_quotes/<id>')
+def movie_quotes(id):
+    response = api_request(f'movie/{id}/quote')
+
+    print(response)
+
+    return render_template('movie_quotes.html', quotes=response['docs'])
+
+
+@app.route('/characters')
+def characters():
+    response = api_request(f'character')
+
+    print(response)
+
+    return render_template('characters.html', characters=response['docs'])
+
+
+@app.route('/chapters')
+def chapters():
+    response = api_request(f'chapter')
+
+    return render_template('chapters.html', chapters=response['docs'])
+
+
+@app.route('/quotes')
+def quotes():
+    response = api_request(f'quote')
+
+    print(response)
+
+    return render_template('quotes.html', quotes=response['docs'])
 
 if __name__ == '__main__':
     app.run(debug=True)
